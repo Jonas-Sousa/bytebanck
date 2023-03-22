a99163f5221a2a653a65a5711d03cacf5109283e
Jonas sousa <jonassousagtr@gmail.com>
2023-03-22 19:43:00 -0300

    feat: Adicionando um setInterval, para a cada 5 segundos ele executar a função conectaAPI.

--- script/scripts.js
+++ script/scripts.js
@@ -14,8 +14,7 @@ const graficoParaDolar = new Chart(graficoDolar, {
 
 async function conectaAPI() {
     const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
+    const response = await conecta.json();
 }
 
-conectaAPI();
+setInterval(() => conectaAPI(), 5000);
